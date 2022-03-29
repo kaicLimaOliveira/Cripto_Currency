3e3e06b930e2950a9e48d7fe481a52900340ca1f
kaicLimaOliveira <kaic10@outlook.com>
2022-03-29 08:40:22 -0300

    Ajustando versão mobile

--- src/components/ListQuotes.vue
+++ src/components/ListQuotes.vue
@@ -1,53 +1,55 @@
 <template>
-  <table class="table">
-    <thead>
-      <tr>
-        <th>Moeda</th>
-        <th>Nome</th>
-        <th>Máximo</th>
-        <th>Mínimo</th>
-        <th>Variação</th>
-        <th></th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr v-for="(quote, key) in props.quotes" :key="key">
-        <td>{{ key }}</td>
-        <td>{{ quote.name }}</td>
-        <td>{{ quote.high }}</td>
-        <td>{{ quote.low }}</td>
-        <td>
-          <span
-            class="label label-rounded text-small"
-            :class="{
-              'label-error': quotes.pctChange < 0,
-              'label-success': quote.pctChange > 0,
-            }"
-          >
-            {{ quote.pctChange }} %
-          </span>
-        </td>
-        <td>
-          <a
-            v-if="!listenQuotes.includes(key)"
-            class="btn btn-primary btn-sm tooltip tooltip-left"
-            data-tooltip="Seguir"
-            @click="$emit('listen', key)"
-          >
-            <i class="icon icon-plus"></i>
-          </a>
-          <a
-            v-else
-            class="btn btn-error btn-sm tooltip tooltip-left"
-            data-tooltip="Remover"
-            @click="$emit('unlisten', key)"
-          >
-            <i class="icon icon-minus"></i>
-          </a>
-        </td>
-      </tr>
-    </tbody>
-  </table>
+  <div class="table-responsive">
+    <table>
+      <thead>
+        <tr>
+          <th>Moeda</th>
+          <th>Nome</th>
+          <th>Máximo</th>
+          <th>Mínimo</th>
+          <th>Variação</th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(quote, key) in props.quotes" :key="key">
+          <td>{{ key }}</td>
+          <td>{{ quote.name }}</td>
+          <td>{{ quote.high }}</td>
+          <td>{{ quote.low }}</td>
+          <td>
+            <span
+              class="label label-rounded text-small"
+              :class="{
+                'label-error': quotes.pctChange < 0,
+                'label-success': quote.pctChange > 0,
+              }"
+            >
+              {{ quote.pctChange }} %
+            </span>
+          </td>
+          <td>
+            <a
+              v-if="!listenQuotes.includes(key)"
+              class="btn btn-primary btn-sm tooltip tooltip-left"
+              data-tooltip="Seguir"
+              @click="$emit('listen', key)"
+            >
+              <i class="icon icon-plus"></i>
+            </a>
+            <a
+              v-else
+              class="btn btn-error btn-sm tooltip tooltip-left"
+              data-tooltip="Remover"
+              @click="$emit('unlisten', key)"
+            >
+              <i class="icon icon-minus"></i>
+            </a>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <script lang="ts" setup>
@@ -66,3 +68,22 @@ const props = defineProps({
 
 const emit = defineEmits(["listen", "unlisten"]);
 </script>
+
+<style scoped>
+  .table-responsive {
+    min-height: .01%;
+    overflow-y: scroll;
+  }
+
+  @media screen and (max-width: 767px) {
+    .table-responsive {
+        width: 100%;
+        margin-bottom: 15px;
+        border: 1px solid #ddd;
+    }
+
+    table {
+      width: 100%;
+    }
+}
+</style>
